ac6ae40e973ae5bd8bdff70b5889701f533a7dde
bogyuuggi <sss0832@naver.com>
2023-08-11 19:02:15 +0900

    commit_BG

--- sample1/src/main/java/com/example/sample1/mapper/UserMapper.java
+++ sample1/src/main/java/com/example/sample1/mapper/UserMapper.java
@@ -1,12 +1,13 @@
 package com.example.sample1.mapper;
 
-import java.util.List;
+import java.util.HashMap;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.sample1.model.Test;
-
 @Mapper
 public interface UserMapper {
-	
+	// 회원가입
+	int insert(HashMap<String, Object> map);
+	// 아이디 중복체크
+	int idCheck(HashMap<String, Object> map);
 }
---
+++ sample1/src/main/java/com/example/sample1/model/User.java
@@ -0,0 +1,20 @@
+package com.example.sample1.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String userEmail;
+	private String userPhone;
+	private String userGender;
+	private String userPwdHint;
+	private String userPwdAnswer;
+	private String userJoinDate;
+	private String userUpDate;
+	private String userLeaveDate;
+	private String userBanTn;
+	
+}
