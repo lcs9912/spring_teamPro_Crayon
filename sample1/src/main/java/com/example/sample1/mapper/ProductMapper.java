d0f53605d5da49af979cb5f3def53e2078a074f4
Son <smo3072@naver.com>
2023-08-16 09:34:23 +0900

    0816

--- sample1/src/main/java/com/example/sample1/mapper/ProductMapper.java
+++ sample1/src/main/java/com/example/sample1/mapper/ProductMapper.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.sample1.model.Test;
+import com.example.sample1.model.Product;
 
 @Mapper
 public interface ProductMapper {
