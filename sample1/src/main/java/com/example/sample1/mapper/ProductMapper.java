6e5ba01ffbc5c796421358ab81f120e940ebbe2d
bogyuuggi <sss0832@naver.com>
2023-08-17 14:46:47 +0900

    test

--- sample1/src/main/java/com/example/sample1/mapper/ProductMapper.java
+++ sample1/src/main/java/com/example/sample1/mapper/ProductMapper.java
@@ -1,5 +1,6 @@
 package com.example.sample1.mapper;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -8,5 +9,12 @@ import com.example.sample1.model.Product;
 
 @Mapper
 public interface ProductMapper {
-	
+	//상품 등록
+	int insertProduct(HashMap<String, Object> map);
+	//상품 이미지 업로드
+	int insertProductImg(HashMap<String, Object> map);
+	//상품 브랜드 조회
+	List<Product> selectBrand(HashMap<String, Object> map);
+	//브랜드 이름 직접입력
+	int insertBrand(HashMap<String, Object> map);
 }
